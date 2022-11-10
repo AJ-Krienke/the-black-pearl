ecef1718a726da120865dc10b73e555de7255fbc
AJ <krienkeaj@gmail.com>
2022-11-10 12:07:57 -0500

    too complicated doing over with components first

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Layout from './components/layout/Layout';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Layout />
-  </React.StrictMode>
-);
+root.render(<React.StrictMode></React.StrictMode>);
