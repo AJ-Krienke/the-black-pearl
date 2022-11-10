a2dccf1f23e55665f499ecd6809e18d6b9d3e5be
AJ <krienkeaj@gmail.com>
2022-11-09 19:12:43 -0500

    Created main layout files and layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,22 @@
-import styles from './Layout.module.css';
+import Header from '../sections/Header/Header';
+import MainContent from '../sections/MainContent/MainContent';
+import Footer from '../sections/Footer/Footer';
+import React from 'react';
 
-const Layout = props => {};
+const Layout = props => {
+  return (
+    <React.Fragment>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <MainContent />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </React.Fragment>
+  );
+};
 
 export default Layout;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Layout from './components/Layout/Layout';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LayoutWrapper />
+    <Layout />
   </React.StrictMode>
 );
