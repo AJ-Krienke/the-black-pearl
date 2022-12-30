76e4cae2daae231667109fdbdf2f5f3b51b1ee6a
AJ Krienke <krienkeaj@gmail.com>
2022-12-30 13:39:32 -0500

    Update logic to conditionally render modal

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { useState, useRef } from 'react';
+import React, { useState, useRef } from 'react';
+
+// ---------- Page Sections
 
 import Hero from './PageSections/HeroSection/Hero';
 import Intro from './PageSections/IntroSection/Intro';
@@ -8,11 +9,21 @@ import Gallery from './PageSections/GallerySection/Gallery';
 import SignupForm from './Forms/SignupForm/SignupForm';
 import Menu from './PageSections/MenuSection/Menu';
 import Contact from './PageSections/ContactSection/Contact/Contact';
+
+// -------End Page Sections
+
+// ---------- Page Elements
+
 import SignupContext from './Contexts/SignupContext';
 import LoginButton from './Components/LoginButton/LoginButton';
+import Modal from './Components/Modal/Modal';
+import EmailPasswordForm from './Forms/EmailPasswordForm/EmailPasswordForm';
+
+// -------End Page Elements
 
 const LandingPage = props => {
   const [isSignedUp, setIsSignedUp] = useState(false);
+  const [modal, setModal] = useState(false);
 
   const signupRef = useRef(null);
   const contactRef = useRef(null);
@@ -27,28 +38,47 @@ const LandingPage = props => {
     contactRef.current.scrollIntoView();
   };
 
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
   return (
     <React.Fragment>
       <SignupContext.Provider value={[isSignedUp, setIsSignedUp]}>
-        <LoginButton />
-        <header>
-          <Hero
-            onContactClick={contactClickHandler}
-            onSignupClick={signupClickHandler}
-          />
-        </header>
-        <main>
-          <Intro />
-          <Events />
-          <Gallery />
-        </main>
-        <aside>
-          <SignupForm ref={signupRef} />
-        </aside>
-        <footer>
-          <Menu />
-          <Contact ref={contactRef} />
-        </footer>
+        {!modal ? (
+          <>
+            <LoginButton onClick={toggleModal} />
+            <header>
+              <Hero
+                onContactClick={contactClickHandler}
+                onSignupClick={signupClickHandler}
+              />
+            </header>
+            <main>
+              <Intro />
+              <Events />
+              <Gallery />
+            </main>
+            <aside>
+              <SignupForm ref={signupRef} />
+            </aside>
+            <footer>
+              <Menu />
+              <Contact ref={contactRef} />
+            </footer>
+          </>
+        ) : (
+          <>
+            <Modal>
+              <EmailPasswordForm
+                toggleModal={toggleModal}
+                text='Log in now.'
+                ref={signupRef}
+                autoFocus={true}
+              />
+            </Modal>
+          </>
+        )}
       </SignupContext.Provider>
     </React.Fragment>
   );
