b4a0d60ab5ce5859c2fc56f0e04172aaf590badf
AJ Krienke <krienkeaj@gmail.com>
2023-01-02 14:05:46 -0500

    Fix logic for Recommendation screen

--- src/PageSections/RecommendationSection/Recommendation.jsx
+++ src/PageSections/RecommendationSection/Recommendation.jsx
@@ -1,12 +1,6 @@
-import { useReducer } from 'react';
+import { useReducer, useState, useEffect } from 'react';
 
 import styles from './Recommendation.module.css';
-
-const options = {
-  type: ['Red', 'White'],
-  characteristic: ['Dry', 'Sweet'],
-  body: ['Light', 'Medium', 'Full'],
-};
 
 const wineList = [
   {
@@ -25,22 +19,64 @@ const wineList = [
   },
   {
     id: 2,
-    name: 'Shiraz',
+    name: 'Syrah',
     type: 'Red',
     characteristic: 'Dry',
     body: 'Full',
   },
   {
     id: 3,
+    name: 'Pinot Noir',
+    type: 'Red',
+    characteristic: 'Dry',
+    body: 'Light',
+  },
+  {
+    id: 4,
+    name: 'Sangiovese',
+    type: 'Red',
+    characteristic: 'Dry',
+    body: 'Full',
+  },
+  {
+    id: 5,
+    name: 'Red Blend',
+    type: 'Red',
+    characteristic: 'Dry',
+    body: 'Full',
+  },
+  {
+    id: 6,
     name: 'Sauvignon Blanc',
+    type: 'White',
+    characteristic: 'Dry',
+    body: 'Full',
+  },
+  {
+    id: 7,
+    name: 'Chardonnay',
     type: 'White',
     characteristic: 'Dry',
     body: 'Medium',
   },
   {
-    id: 4,
-    name: 'Chardonnay',
+    id: 8,
+    name: 'Pinot Grigio',
     type: 'White',
+    characteristic: 'Dry',
+    body: 'Light',
+  },
+  {
+    id: 9,
+    name: 'Prosecco',
+    type: 'Sparkling',
+    characteristic: 'Dry',
+    body: 'Light',
+  },
+  {
+    id: 10,
+    name: 'Champagne',
+    type: 'Sparkling',
     characteristic: 'Dry',
     body: 'Full',
   },
@@ -48,6 +84,8 @@ const wineList = [
 
 const wineListReducer = (state, action) => {
   switch (action.type) {
+    case 'reset':
+      return wineList;
     case 'type':
       return state.filter(currentWine => currentWine.type === action.value);
     case 'characteristic':
@@ -63,8 +101,32 @@ const wineListReducer = (state, action) => {
 
 const Recommendation = props => {
   const [wines, updateWineList] = useReducer(wineListReducer, wineList);
+  const [options, setOptions] = useState({
+    type: [],
+    characteristic: [],
+    body: [],
+  });
   // Recommendation app
   // this should be a database based query
+
+  useEffect(() => {
+    const typeList = [];
+    const characteristicList = [];
+    const bodyList = [];
+
+    wines.forEach(wine => {
+      if (!typeList.includes(wine.type)) typeList.push(wine.type);
+      if (!characteristicList.includes(wine.characteristic))
+        characteristicList.push(wine.characteristic);
+      if (!bodyList.includes(wine.body)) bodyList.push(wine.body);
+    });
+
+    setOptions({
+      type: [...typeList],
+      characteristic: [...characteristicList],
+      body: [...bodyList],
+    });
+  }, [wines]); // This will re-run anytime the wines list is changed
 
   const handleChoiceClick = event => {
     updateWineList({
@@ -73,22 +135,27 @@ const Recommendation = props => {
     });
   };
 
+  const handleResetClick = event => updateWineList({ type: 'reset' });
+
   return (
     <section>
       <h2>Recommendations</h2>
       <h3>
         Select what you like and we'll find the best choice for your taste
       </h3>
-      {wines.map(wine => {
-        return (
-          <p
-            key={wine.id}
-            className={styles.p}
-          >
-            {wine.name}
-          </p>
-        );
-      })}
+
+      <ul className={styles.list}>
+        {wines.map(wine => {
+          return (
+            <li
+              key={wine.id}
+              className={styles['list-item']}
+            >
+              {wine.name}
+            </li>
+          );
+        })}
+      </ul>
 
       <div className={styles.fieldsets}>
         <fieldset>
@@ -128,6 +195,12 @@ const Recommendation = props => {
           ))}
         </fieldset>
       </div>
+      <button
+        className={styles.reset}
+        onClick={handleResetClick}
+      >
+        Reset
+      </button>
     </section>
   );
 };
