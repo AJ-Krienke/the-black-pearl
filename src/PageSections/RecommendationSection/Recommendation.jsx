9b338cb5febf874929a0ebcf737e2b7b8a08d3e9
AJ Krienke <krienkeaj@gmail.com>
2023-02-01 12:56:18 -0500

    add grid layout to recommendation component

--- src/PageSections/RecommendationSection/Recommendation.jsx
+++ src/PageSections/RecommendationSection/Recommendation.jsx
@@ -1,3 +1,11 @@
+// ------ TODO -----------
+//
+// Create context with DB info
+// Move wine list to DB
+// Fetch list from DB
+// Add Cocktail List to DB
+//
+// ------ END TODO -------
 import { useReducer, useState, useEffect } from 'react';
 
 import styles from './Recommendation.module.css';
@@ -23,7 +31,7 @@ const wineList = [
     type: 'Red',
     characteristic: 'Dry',
     body: 'Full',
-  },
+  // },
   {
     id: 3,
     name: 'Pinot Noir',
@@ -110,23 +118,43 @@ const Recommendation = props => {
   // this should be a database based query
 
   useEffect(() => {
+    // Displayed list needs to update
+    // each time a user makes a
+    // selection so we create the lists
+    // of wine type, character, and
+    // body
     const typeList = [];
     const characteristicList = [];
     const bodyList = [];
 
     wines.forEach(wine => {
+      // We use seperate if statements
+      // so that every wine in the list
+      // is evaluated for all of it's
+      // attributes. if the attribute
+      // isn't already in the array,
+      // add it for each wine in the
+      // wine list
       if (!typeList.includes(wine.type)) typeList.push(wine.type);
       if (!characteristicList.includes(wine.characteristic))
         characteristicList.push(wine.characteristic);
       if (!bodyList.includes(wine.body)) bodyList.push(wine.body);
     });
 
+    // Once our arrays are complete
+    // we update the state to reflect
+    // the updated lists
     setOptions({
       type: [...typeList],
       characteristic: [...characteristicList],
       body: [...bodyList],
     });
-  }, [wines]); // This will re-run anytime the wines list is changed
+  }, [wines]); // This will re-run
+  // anytime the wines list is changed
+  // causing the buttons on screen to
+  // rerender which will always
+  // reflect the options available to
+  // the user for their given choices
 
   const handleChoiceClick = event => {
     updateWineList({
@@ -139,68 +167,80 @@ const Recommendation = props => {
 
   return (
     <section>
-      <h2>Recommendations</h2>
+      <h2>Recommendations.</h2>
       <h3>
-        Select what you like and we'll find the best choice for your taste
+        Select what you like and we'll find the best choice for your personal
+        taste.
       </h3>
 
-      <ul className={styles.list}>
-        {wines.map(wine => {
-          return (
-            <li
-              key={wine.id}
-              className={styles['list-item']}
-            >
-              {wine.name}
-            </li>
-          );
-        })}
-      </ul>
-
-      <div className={styles.fieldsets}>
-        <fieldset>
-          <legend>Type</legend>
-          {options.type.map(choice => (
-            <button
-              key={choice}
-              className='type'
-              onClick={handleChoiceClick}
-            >
-              {choice}
-            </button>
-          ))}
-        </fieldset>
-        <fieldset>
-          <legend>Characteristic</legend>
-          {options.characteristic.map(choice => (
-            <button
-              key={choice}
-              className='characteristic'
-              onClick={handleChoiceClick}
-            >
-              {choice}
-            </button>
-          ))}
-        </fieldset>
-        <fieldset>
-          <legend>Body</legend>
-          {options.body.map(choice => (
-            <button
-              key={choice}
-              className='body'
-              onClick={handleChoiceClick}
-            >
-              {choice}
-            </button>
-          ))}
-        </fieldset>
+      <div className={`${styles['recommendation-wrapper']} grid grid--2col`}>
+        <div
+          className={styles['list-wrapper']}
+          role='presentation'
+        >
+          <ul className={styles.list}>
+            {wines.map(wine => {
+              return (
+                <li
+                  key={wine.id}
+                  className={styles['list-item']}
+                >
+                  {wine.name}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div
+          className={styles['button-wrapper']}
+          role='presentation'
+        >
+          <div className={styles.fieldsets}>
+            <fieldset>
+              <legend>Type</legend>
+              {options.type.map(choice => (
+                <button
+                  key={choice}
+                  className='type'
+                  onClick={handleChoiceClick}
+                >
+                  {choice}
+                </button>
+              ))}
+            </fieldset>
+            <fieldset>
+              <legend>Characteristic</legend>
+              {options.characteristic.map(choice => (
+                <button
+                  key={choice}
+                  className='characteristic'
+                  onClick={handleChoiceClick}
+                >
+                  {choice}
+                </button>
+              ))}
+            </fieldset>
+            <fieldset>
+              <legend>Body</legend>
+              {options.body.map(choice => (
+                <button
+                  key={choice}
+                  className='body'
+                  onClick={handleChoiceClick}
+                >
+                  {choice}
+                </button>
+              ))}
+            </fieldset>
+          </div>
+        </div>
+        <button
+          className={styles.reset}
+          onClick={handleResetClick}
+        >
+          Reset
+        </button>
       </div>
-      <button
-        className={styles.reset}
-        onClick={handleResetClick}
-      >
-        Reset
-      </button>
     </section>
   );
 };
